f3a51b3dc81c6a1cc3db0e91cb4a642764c09a4f
adullakadar <abdhullahkadar06@gmail.com>
2025-12-04 13:06:46 +0400

    -06 easy frfr

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,len) {
+  newstr=''
+  if (len<0){
+    return 'ERROR';
+  }
+  for (let i=1; i<=len; i++){
+    newstr+=string;
+  }
+  return newstr;
 };
 
 // Do not edit below this line
