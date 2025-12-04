545d5478fef679482fb032a14699afdb17fa7cd6
adullakadar <abdhullahkadar06@gmail.com>
2025-12-04 14:02:29 +0400

    -ts took way too long

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr,...toPop) {
+    let filteredArr = [];
+    for (let i=0; i < arr.length; i++){
+      if (!toPop.includes(arr[i])){
+        filteredArr.push(arr[i])
+      };
+    };
+    return filteredArr;
+  }
 
 // Do not edit below this line
 module.exports = removeFromArray;
